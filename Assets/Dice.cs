bf6003ee9d545ef5101e51c2b07414d28ac5353c
stupid999 <aierzhiguang2@gmail.com>
2019-08-23 19:41:55 +0800

    输入格子名 空格鍵可以投色子 並會顯示色子數，玩家所在格子數，格子名

---
+++ Assets/Dice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    int DiceNum;
+    int playerNum;
+    [SerializeField]
+    Map map;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RollDice();
+            playerNum += DiceNum;
+            DiceNum = 0;
+            if (playerNum > 35)
+            {
+                playerNum = playerNum % 35;
+            }
+            Debug.Log("playerNum = " + playerNum);
+            Debug.Log("placeName = " + map.placeName[playerNum]);
+        }
+    }
+
+    public void RollDice()
+    {
+        DiceNum = Random.Range(1, 7);
+        Debug.Log("DiceNum = " + DiceNum);
+    }
+}
---
+++ Assets/Map.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public string[] placeName = new string[36];
+    string start = "Start";
+    string dairyLife = "Dairy Life";
+    string house = "House";
+    string bussinessRisk = "Bussiness Risk";
+    string futureMarket = "Future Market";
+    string marketRisk = "Market Risk";
+    string stockMarket = "Stock Market";
+    string child = "Child";
+    string investmentOptions="Investment Options";
+    string bank = "Bank";
+    string boundNFund = "Bound & Fund";
+    string law = "Law";
+    string cashDividend = "Cash Dividend";
+    string hospital = "Hospital";
+    string carShop = "Car Shop";
+
+    private void Start()
+    {
+        // 第一行
+        placeName[0] = start;
+        placeName[1] = dairyLife;
+        placeName[2] = house;
+        placeName[3] = bussinessRisk;
+        placeName[4] = futureMarket;
+        placeName[5] = marketRisk;
+        placeName[6] = stockMarket;
+        placeName[7] = child;
+        placeName[8] = investmentOptions;
+        placeName[9] = dairyLife;
+
+        // 第二行
+        placeName[10] = bank;
+        placeName[11] = stockMarket;
+        placeName[12] = house;
+        placeName[13] = boundNFund;
+        placeName[14] = stockMarket;
+        placeName[15] = investmentOptions;
+        placeName[16] = dairyLife;
+        placeName[17] = marketRisk;
+
+        // 第三行
+        placeName[18] = law;
+        placeName[19] = "IDK!!!!!!";
+        placeName[20] = futureMarket;
+        placeName[21] = bussinessRisk;
+        placeName[22] = boundNFund;
+        placeName[23] = house;
+        placeName[24] = dairyLife;
+        placeName[25] = stockMarket;
+        placeName[26] = marketRisk;
+        placeName[27] = cashDividend;
+
+        // 第四行
+        placeName[28] = hospital;
+        placeName[29] = futureMarket;
+        placeName[30] = dairyLife;
+        placeName[31] = bussinessRisk;
+        placeName[32] = house;
+        placeName[33] = investmentOptions;
+        placeName[34] = marketRisk;
+        placeName[35] = carShop;
+    }
+}
